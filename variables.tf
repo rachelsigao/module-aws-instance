002ac36c183ca08c5b7984146798d7d3470f226a
rachelsigao <rachelsigao@gmail.com>
2026-04-30 14:25:20 -0400

    Terraform

--- variables.tf
+++ variables.tf
@@ -9,9 +9,9 @@ variable "instance_type" {
     type = string
     description = "Instance size"
 
-    validation {
-        condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)
-        error_message = "Valid values for instance_type are: t3.micro, t3.small, t3.medium"
+    validation { # validation block to restrict instance types besides the defined values.
+        condition     = contains(["t3.micro", "t3.small", "t3.medium"], var.instance_type)  
+        error_message = "Valid values for instance_type are: t3.micro, t3.small, t3.medium" 
     } 
 }
 
